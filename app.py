82b27a2ca61e78aad1ba1bf20787af53368c5d15
Fangzhou Zhang <1298731264@qq.com>
2023-07-31 18:50:35 +0100

    Update the page structure to display images

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import streamlit as st
 import openai
-import re
 import os
 import requests
 import json
 from PIL import Image
 from io import BytesIO
 import time
+import numpy as np
+from concurrent.futures import ThreadPoolExecutor, as_completed
 import gpt_prompt
 
 openai.api_key = os.getenv('OPENAI_API_KEY')
@@ -28,7 +29,7 @@ def imagine(prompt):
     "ignorePrefilter": "false"
     })
 
-    def check_task_status(url):  # 这里添加 url 参数
+    def check_task_status(url):
         while True:
             response_result = requests.request("GET", url, headers=headers)
             if response_result.status_code == 200:
@@ -59,112 +60,138 @@ def imagine(prompt):
                 attempts += 1
     raise ValueError("Unable to get valid image URL after multiple attempts")
 
-def main():
+# get session state
+if 'init_prompt' not in st.session_state:
+    st.session_state.init_prompt = ''
+if 'prompts' not in st.session_state:
+    st.session_state.prompts = []
+if 'prompt_history' not in st.session_state:
+    st.session_state.prompt_history = []
+if 'images' not in st.session_state:
+    st.session_state.images = []
+if 'image_history' not in st.session_state:
+    st.session_state.image_history = []
+if 'selection_history' not in st.session_state:
+    st.session_state.selection_history = []
+if 'selected_images' not in st.session_state:
+    st.session_state.selected_images = []
+if 'scores' not in st.session_state:
+    st.session_state.scores = []
+if 'round_count' not in st.session_state:
+    st.session_state.round_count = 0
+if 'keyword_mat' not in st.session_state:
+    st.session_state.keyword_mat = None
 
-    if 'all_prompt_lists' not in st.session_state:
-        st.session_state['all_prompt_lists'] = [None] * 1000
-    
-    if 'all_selected_lists' not in st.session_state:
-        st.session_state['all_selected_lists'] = [None] * 1000
+st.title("Genjourney:dna:")
 
-    if 'init_prompt' not in st.session_state:
-        st.session_state['init_prompt'] = ""
+# choose the mj mode
+option = st.radio('**Choose a Midjourney mode:**', ('Midjourney Model V5.2', 'Niji Model V5'))
 
-    if 'count' not in st.session_state:
-        st.session_state['count'] = 1
+# input init prompt and generate prompts of generation 0
+st.session_state.init_prompt = st.text_input("**Enter your initial prompt**", value=st.session_state.init_prompt)
 
-    if 'choices' not in st.session_state:
-        st.session_state['choices'] = [None] * 1000
+if st.button("**Generate prompts & images**"):
 
-    if 'img_cache' not in st.session_state:
-        st.session_state['img_cache'] = {}
-    
-    if 'keyword_mat' not in st.session_state:
-        st.session_state['keyword_mat'] = None
+    prompt_mat, keyword_mat = gpt_prompt.generate(st.session_state['init_prompt'], 5)
+    st.session_state.keyword_mat = keyword_mat
+    st.session_state.prompts = prompt_mat
 
-    st.title("Genjourney:dna:")
+if np.size(st.session_state.prompts) > 0:
+    # show the lastest images
+    st.markdown("# Current Images")
 
-    option = st.radio('**Choose a Midjourney mode:**', ('Midjourney Model V5.2', 'Niji Model V5'))
+    image_list = []
+    for i in range(len(st.session_state.prompts)):
+        prompt = gpt_prompt.list2str(st.session_state.prompts[i], option)
+        st.write(i+1, prompt)
+        img = imagine(prompt)
+        st.image(img, caption=prompt)
+        image_list.append(img)
+    st.session_state.images = image_list
 
-    st.session_state['init_prompt'] = st.text_input("**Enter your initial prompt**")
-    if st.button("**Generate prompts & images**", key="init_list_button"):
-        prompt_mat, keyword_mat = gpt_prompt.generate(st.session_state['init_prompt'], 5)
-        st.session_state['keyword_mat'] = keyword_mat
-        st.session_state['all_prompt_lists'][0] = prompt_mat
+    # # multi-thread
+    # def get_image(prompt):
+    #     img = imagine(prompt)
+    #     return img, prompt
 
-    if st.session_state['all_prompt_lists'][0] is not None:
-        st.write("**Initial generated prompts:**")
-        for i in range(5):
-            prompt = gpt_prompt.list2str(st.session_state['all_prompt_lists'][0][i], option)
-            st.write(i + 1, prompt)
+    # # results = []
+    # # with ThreadPoolExecutor(max_workers=5) as executor:
+    # #     futures = [executor.submit(get_image, gpt_prompt.list2str(prompt, option)) for prompt in st.session_state.prompts]
+    # #     progress_bar = st.progress(0)
+    # #     for i, future in enumerate(as_completed(futures)):
+    # #         results.append(future.result())
+    # #         progress_bar.progress((i+1) / len(futures))
 
-            img = st.session_state['img_cache'].get(prompt)
-            if img is None:
-                img = imagine(prompt)
-                st.session_state['img_cache'][prompt] = img
-            st.write(img)
-            st.image(img)
+    # # for i, (img, prompt) in enumerate(results, start=1):
+    # #     st.image(img, caption=f"{i}. {prompt}")
 
-        for i in range(st.session_state['count']):
-            container = st.container()
-            prompt_mat = st.session_state['all_prompt_lists'][i]
-            selected_nums = container.multiselect(f"**Select two prompts as parent prompts**", range(1, 6), key=f"multi_select_{i}")
-            rate_list = []
+    # with ThreadPoolExecutor(max_workers=5) as executor:
+    #     futures = [executor.submit(get_image, gpt_prompt.list2str(prompt, option)) for prompt in st.session_state.prompts]
+    #     progress_bar = st.progress(0)
+    #     results = []
+    #     for i, future in enumerate(as_completed(futures)):
+    #         img, prompt = future.result()
+    #         results.append((img, prompt))
+    #         progress_bar.progress((i + 1) / len(futures))
+    #     progress_bar.progress(1.0)
 
-            if len(selected_nums) == 2:
-                selected_prompts = [prompt_mat[selected_nums[0] - 1], prompt_mat[selected_nums[1] - 1]]
+    # image_list = []
+    # for i, (img, prompt) in enumerate(results, start=1):
+    #     st.write(f"{i}. {prompt}")
+    #     st.image(img, caption=prompt)
+    #     image_list.append(img)
 
-                subject_rate = st.slider('rate the subject', min_value=0, max_value=10, key=f"slider_subject_{i}")
-                style_rate = st.slider('rate the style', min_value=0, max_value=10, key=f"slider_style_{i}")
-                color_rate = st.slider('rate the color', min_value=0, max_value=10, key=f"slider_color_{i}")
-                angle_rate = st.slider('rate the angle', min_value=0, max_value=10, key=f"slider_angle_{i}")
-                light_rate = st.slider('rate the light', min_value=0, max_value=10, key=f"slider_light_{i}")
-                renderer_rate = st.slider('rate the renderer', min_value=0, max_value=10, key=f"slider_renderer_{i}")
+    # st.session_state.images = image_list
+
+    with st.form(key='my_form'):
+        # let user to choose two prompts as parent prompts
+        st.session_state.selected_images = st.multiselect('Select two prompts as parent prompts', range(1, 6), key=f"select_{st.session_state.round_count}")
+        
+        # let user to rate the art styles
+        # if st.session_state.selected_images and len(st.session_state.selected_images) == 2:
+        if st.session_state.selected_images:
+            if len(st.session_state.selected_images) != 2:
+                st.warning('Please select exactly two images and set scores for them.')
+            else:
+                subject_rate = st.slider('rate the subject', min_value=0, max_value=10, value=5, key=f"slider_subject_{st.session_state.round_count}_{i}")
+                style_rate = st.slider('rate the style', min_value=0, max_value=10, value=5, key=f"slider_style_{st.session_state.round_count}_{i}")
+                color_rate = st.slider('rate the color', min_value=0, max_value=10, value=5, key=f"slider_color_{st.session_state.round_count}_{i}")
+                angle_rate = st.slider('rate the angle', min_value=0, max_value=10, value=5, key=f"slider_angle_{st.session_state.round_count}_{i}")
+                light_rate = st.slider('rate the light', min_value=0, max_value=10, value=5, key=f"slider_light_{st.session_state.round_count}_{i}")
+                renderer_rate = st.slider('rate the renderer', min_value=0, max_value=10, value=5, key=f"slider_renderer_{st.session_state.round_count}_{i}")
                 rate_list = [subject_rate, style_rate, color_rate, angle_rate, light_rate, renderer_rate]
+                st.session_state.scores = rate_list
+        
+        # submit button
+        submit_button = st.form_submit_button(label='Submit for Next Generation')
 
-            button_key = f"button_{i}"
-            if button_key not in st.session_state:
-                st.session_state[button_key] = False
+    if submit_button:
+        if len(st.session_state.selected_images) != 2:
+            st.warning('Please select exactly two images and set scores for them.')
+        else:
+            # store history
+            st.session_state.prompt_history.append(st.session_state.prompts)
+            st.session_state.image_history.append(st.session_state.images)
+            st.session_state.selection_history.append(st.session_state.selected_images)
 
-            if len(selected_nums) == 2:
-                if st.button(f"**Generate child prompts & images**", key=button_key):
-                    st.session_state[f'{button_key}_clicked'] = True
-                
-            if len(selected_nums) == 2 and st.session_state.get(f'{button_key}_clicked', False):
-                st.session_state['choices'][i] = selected_prompts
-                child_prompts = gpt_prompt.genetic(selected_prompts[0], selected_prompts[1], rate_list=rate_list, keyword_mat=st.session_state['keyword_mat'])
-                st.session_state['all_prompt_lists'][i+1] = child_prompts
-                st.session_state['all_selected_lists'][i] = selected_prompts
-                if i + 1 == st.session_state['count']:
-                    st.session_state['count'] += 1
-                    # st.experimental_rerun()
-            
-            if st.session_state['choices'][i] is not None:
-                st.write(f"**Your selcetion from Generation {i}:**")
-                for j in range(2):
-                    st.write(j + 1, gpt_prompt.list2str(st.session_state['all_selected_lists'][i][j], option))
-                st.write("\n")
-                st.write(f"**Generation {i + 1}:**")
-                for j in range(5):
-                    prompt = gpt_prompt.list2str(st.session_state['all_prompt_lists'][i + 1][j], option)
-                    st.write(j + 1, prompt)
+            # generate prompts of next generation
+            prompt_A = st.session_state.prompts[st.session_state.selected_images[0]-1]
+            prompt_B = st.session_state.prompts[st.session_state.selected_images[1]-1]
+            st.session_state.prompts = gpt_prompt.genetic(prompt_A, prompt_B, st.session_state.scores, st.session_state.keyword_mat)
 
-                    img = st.session_state['img_cache'].get(prompt)
-                    if img is None:
-                        img = imagine(prompt)
-                        st.session_state['img_cache'][prompt] = img
-                    st.write(img)
-                    st.image(img)
-                    
-    if st.button(':leftwards_arrow_with_hook::violet[**Try another initial prompt**]', key="refresh"):
-        st.session_state['all_prompt_lists'] = [None] * 1000
-        st.session_state['all_selected_lists'] = [None] * 1000
-        st.session_state['init_prompt'] = ""
-        st.session_state['count'] = 1
-        st.session_state['choices'] = [None] * 1000
-        st.session_state['img_cache'] = {}
-        st.session_state['keyword_mat'] = None
-        st.experimental_rerun()
+            # increase round count
+            st.session_state.round_count += 1
 
-if __name__ == '__main__':
-    main()
+            # clear selection and scores
+            st.session_state.selected_images = []
+            st.session_state.scores = []
+
+    # show history and selection on sidebar
+    if st.session_state.image_history:
+        st.sidebar.markdown("# History")
+        for i, (past_images, selected) in enumerate(zip(st.session_state.image_history, st.session_state.selection_history)):
+            st.sidebar.markdown(f"## Generation {i}")
+            for idx, img in enumerate(past_images):
+                prompt = gpt_prompt.list2str(st.session_state.prompt_history[i][idx], option)
+                caption = prompt + (f" - Selected" if idx in selected else "")
+                st.sidebar.image(img, caption=caption)
