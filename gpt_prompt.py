87da9fd0141347b137a2bd398f9c9cddcb979674
Fangzhou Zhang <1298731264@qq.com>
2023-08-18 14:47:44 +0100

    Set temperature=1.0

--- gpt_prompt.py
+++ gpt_prompt.py
@@ -19,7 +19,7 @@ def get_completion(prompt, model='gpt-3.5-turbo'):
             response = openai.ChatCompletion.create(
                 model=model,
                 messages=messages,
-                temperature=1.5,
+                temperature=1.0,
             )
             return response['choices'][0]['message']['content']
 
